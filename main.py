af2749251815fb551ec95ec6410253b654250720
jonathan <oregonjonathan@gmail.com>
2024-09-03 21:21:20 -0700

    add circle shape script

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
